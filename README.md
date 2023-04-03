dd8af8b663bd817cd0be5192a0a7f67f80e1519f
xiyao <futuretech6@foxmail.com>
2023-04-03 22:31:15 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Rain Reminder
+
+Get an email if it will rain tomorrow.
+
+## How to use
+
+1. fork this repo
+2. acquire an API key at https://home.openweathermap.org/api_keys and fill it as a GitHub secret with the name "APIKEY"
+3. change the ["config.toml"](/config.toml) to match your location(s), you can use https://www.google.com/maps/@{lat},{lon} to locate
+
+There will be an email sent by the GitHub Actions bot to you at 18:00 if it's rainy tomorrow.
---
+++ main.py
@@ -0,0 +1,54 @@
+import json
+import os
+from datetime import datetime, timedelta
+from typing import Dict, List
+
+import requests
+import toml
+
+config = toml.load("config.toml")
+locations = config["location"]
+apikey = os.environ["APIKEY"]
+
+location_results: Dict[str, List[datetime]] = {}
+
+for location in locations:
+    longitude = locations[location]["longitude"]
+    latitude = locations[location]["latitude"]
+    local_time = datetime.utcnow() + timedelta(hours=longitude // 15)
+    tomorrow = local_time.date() + timedelta(days=1)
+    response = requests.get(
+        url="http://api.openweathermap.org/data/2.5/forecast?lat={}&lon={}&appid={}".format(
+            latitude,
+            longitude,
+            apikey,
+        )
+    )
+    info = json.loads(response.text)
+    if info["cod"] != "200":
+        print(
+            "Request error, error code: {}, message: {}".format(
+                info["cod"], info["message"]
+            )
+        )
+        exit(255)
+
+    for forecast in info["list"]:
+        forecast_time = datetime.fromtimestamp(forecast["dt"])
+        if forecast_time.date() != tomorrow:
+            continue
+        if forecast["weather"][0]["main"] == "Rain":
+            if location not in location_results:
+                location_results[location] = []
+            location_results[location].append(forecast_time)
+
+if len(location_results) != 0:
+    for location in location_results:
+        print("{} will be rainy at ".format(location), end="")
+        time_str: str = ""
+        for dt in location_results[location]:
+            time_str += "{}:{:02}, ".format(dt.hour, dt.minute)
+        print(time_str.rstrip(", "))
+    exit(2)  # exit to make actions alarm
+else:
+    print("sunny everywhere, happy!")
